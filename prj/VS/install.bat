b23712b01ced75f46ce795d74a12dd868483e225
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2021-07-13 12:04:42 +0200

    Adding install.bat

---
+++ prj/VS/install.bat
@@ -0,0 +1,4 @@
+mkdir "%CD%\..\..\bin\VS"
+copy "%CD%\..\..\out\VS\download.exe" "%CD%\..\..\bin\VS\download.exe"
+copy "%CD%\..\..\out\VS\ArgsParser.dll" "%CD%\..\..\bin\VS\ArgsParser.dll"
+copy "%CD%\..\..\out\VS\downloadgui.exe" "%CD%\..\..\bin\VS\downloadgui.exe"
